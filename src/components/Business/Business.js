fe60966fafd7eda8c80f8cd7144c739fd39df08e
Paul Leddy <paull@manifestwebdesign.com>
2017-11-04 11:56:03 -0700

    Use props to pass info down component chain

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,36 +1,24 @@
 import React from 'react';
 import './Business.css';
 
-let business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-}
-
 class Business extends React.Component {
   render() {
     return (
       <div className="Business">
         <div className="image-container">
-          <img src='{business.imageSrc}' alt=''/>
+          <img src={this.props.a_business.imageSrc} alt=''/>
           </div>
-            <h2>{business.name}</h2>
+            <h2>{this.props.a_business.name}</h2>
             <div className="Business-information">
             <div className="Business-address">
-              <p>{business.address}</p>
-              <p>{business.city}</p>
-              <p>{business.state} {business.zip}</p>
+              <p>{this.props.a_business.address}</p>
+              <p>{this.props.a_business.city}</p>
+              <p>{this.props.a_business.state} {this.props.a_business.zip}</p>
             </div>
             <div className="Business-reviews">
-              <h3>{business.category}</h3>
-              <h3 className="rating">{business.rating} stars}</h3>
-              <p>{business.reviewCount} reviews</p>
+              <h3>{this.props.a_business.category}</h3>
+              <h3 className="rating">{this.props.a_business.rating} stars</h3>
+              <p>{this.props.a_business.reviewCount} reviews</p>
           </div>
         </div>
       </div>
